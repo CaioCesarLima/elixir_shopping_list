02ce4e075e700548a057945392c90a898d323ffb
CaioCesarLima <cesarlimacaio@gmail.com>
2022-05-09 21:42:13 -0300

    add tailwind css to project

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :shopping_list, ShoppingListWeb.Endpoint,
   secret_key_base: "E6ruUCwjROcVQrC7Co3UQr7oR/CGn5UJskdycPUCZyEAQ19419QAgqjXQEenN66A",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
